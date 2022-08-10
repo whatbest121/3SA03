a742b1287d934dcda7b28dee48f0a38c543d3d28
Chualchai Apichatitiworn <whatbest@MacBook-Pro-khxng-Chualchai.local>
2022-08-10 22:10:00 +0700

    part 4
    part 4

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import WordCard from './WordCard';
+
+const word = "Hello";
 function App() {
  return (
- <div>
- Hello World
- </div>
+  <div>
+    <WordCard value={word}/>
+  </div>
  );
 }
+
 export default App;
